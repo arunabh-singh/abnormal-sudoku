356c119a6b67909090158fa2c493cc5068574040
Arunabh Singh <Arunabh Singh>
2017-04-16 16:44:08 -0500

    Initial attempt.

--- Sudoku.go
+++ Sudoku.go
@@ -6,22 +6,36 @@
 #Date: 4/16/17
 #Honor Statement: I pledge on my honor that I have neither
 #given nor received unauthorized aid on this assignment.
-*/
-/*
-#Name: 
-#Email: 
-#VUnet ID: 
+
+#Name: Arunabh Singh
+#Email: arunabh.singh@vanderbilt.edu
+#VUnet ID: singha4
 #Class: CS3270
 #Date: 4/16/17
 #Honor Statement: I pledge on my honor that I have neither
 #given nor received unauthorized aid on this assignment.
 */
 
-
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io/ioutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	dat, err := ioutil.ReadFile("sudoku-sample.txt")
+	if err != nil { 
+		panic(err) 
+	}
+	fmt.Print(string(dat))
+
+	f, err := os.Open("/tmp/dat")	
+	if err != nil { 
+		panic(err) 
+	}
+
+	var board [9][9]int
+	
+	
 }
